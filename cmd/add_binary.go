e39d2e0619824dee205058a01482c0ea65b97a7e
Sivaram <sivaram.codfolio@gmail.com>
2025-02-22 20:29:01 +0530

    updates the add_binary

--- cmd/add_binary.go
+++ cmd/add_binary.go
@@ -5,36 +5,28 @@ import (
 )
 
 func addBinary(a string, b string) string {
-	la, lb := len(a), len(b)
-	maxLen := la
-	if lb > la {
-		maxLen = lb
-	}
-
-	result := make([]byte, 0, maxLen+1)
+	result := ""
 	carry := 0
-	i, j := la-1, lb-1
+	i, j := len(a)-1, len(b)-1
 
 	for i >= 0 || j >= 0 || carry > 0 {
 		sum := carry
+
 		if i >= 0 {
 			sum += int(a[i] - '0')
 			i--
 		}
+
 		if j >= 0 {
 			sum += int(b[j] - '0')
 			j--
 		}
 
-		result = append(result, byte(sum%2)+'0')
+		result = string(sum%2+'0') + result
 		carry = sum / 2
 	}
 
-	for k, n := 0, len(result); k < n/2; k++ {
-		result[k], result[n-1-k] = result[n-1-k], result[k]
-	}
-
-	return string(result)
+	return result
 }
 
 func main() {
