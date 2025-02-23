59e2e6a526e7b9f058468ceca8c73980d472ff8f
Sivaram <sivaram.codfolio@gmail.com>
2025-02-23 21:17:35 +0530

    logest_pallindromic_string.go

---
+++ cmd/longest_pallindromic_string.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func expandAroundCenter(s string, left, right int) (int, int) {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return left + 1, right - 1
+}
+
+func longestPalindrome(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		l1, r1 := expandAroundCenter(s, i, i)
+
+		l2, r2 := expandAroundCenter(s, i, i+1)
+
+		if r1-l1 > end-start {
+			start, end = l1, r1
+		}
+		if r2-l2 > end-start {
+			start, end = l2, r2
+		}
+	}
+	return s[start : end+1]
+}
+
+func main() {
+	s := "babad"
+	fmt.Println("Longest Palindromic Substring:", longestPalindrome(s))
+}
