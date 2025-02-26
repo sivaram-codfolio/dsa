5e6d952a367aaf2dd18b3989954315fce83f4f85
Sivaram <sivaram.codfolio@gmail.com>
2025-02-26 22:49:07 +0530

    quick sort patern added

---
+++ cmd/sort/quick.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func quickSort(arr []int, low, high int) {
+	if low < high {
+		pivot := partition(arr, low, high)
+		quickSort(arr, low, pivot-1)
+		quickSort(arr, pivot+1, high)
+	}
+}
+
+func partition(arr []int, low, high int) int {
+	pivot := arr[high]
+	i := low - 1
+
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return i + 1
+}
+
+func main() {
+	nums := []int{3, 1, 4, 1, 5, 9, 2, 6}
+	quickSort(nums, 0, len(nums)-1)
+	fmt.Println(nums)
+}
