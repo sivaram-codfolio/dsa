e44d40019c96a6ca1c7070e203e0ac1a57f06c0b
Sivaram <sivaram.codfolio@gmail.com>
2025-03-23 11:56:29 +0530

    maximum_depth_of_binary_tree - pattern added

---
+++ cmd/maximum_depth_of_binary_tree.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+
+	return 1 + max(leftDepth, rightDepth)
+}
+
+func main() {
+	root := &TreeNode{3,
+		&TreeNode{9, nil, nil},
+		&TreeNode{20,
+			&TreeNode{15, nil, nil},
+			&TreeNode{7, nil, nil},
+		},
+	}
+
+	fmt.Println(maxDepth(root)) // Output: 3
+}
