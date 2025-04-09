7ed87f927b38d5da98d061013c909a9fcfbc5d41
Sivaram <sivaram.codfolio@gmail.com>
2025-04-10 00:21:10 +0200

    rotate_image - pattern added

---
+++ cmd/rotate_image.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+
+	// Transpose the matrix
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+
+	// Reverse each row
+	for i := 0; i < n; i++ {
+		for j := 0; j < n/2; j++ {
+			matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+		}
+	}
+}
+
+func main() {
+	matrix := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	rotate(matrix)
+	fmt.Println(matrix)
+}
